2424ec73ff2eb724026d0c6a4e0879e3b4c9f0cc
digzom <dicksonmelo@gmail.com>
2020-08-06 06:13:12 -0300

    Faltando ainda a função de parar o evento ao clicar novamente.

---
+++ content.js
@@ -0,0 +1,23 @@
+const interval = setInterval(() => {
+   const header = document.querySelector('._1QUKR')
+   if (header) {
+      console.log(header)
+      clearInterval(interval)
+
+      const button = document.createElement("button")
+      button.innerHTML = '1.5'
+      button.classList.add('twoTimesButton')
+
+      button.addEventListener('click', () => {
+         const audios = document.querySelectorAll('audio')
+         console.log(audios)
+         audios.forEach((audio) => {
+            console.log(audio)
+            audio.playbackRate = 1.5
+         })
+      })
+
+      header.appendChild(button)
+   }
+}, 1000)
+
